2e690b7cf2dfdb0eb429b0dd42d53a1f59d4d0b2
Sirupsen <sirup@sirupsen.dk>
2010-05-19 21:24:06 +0200

    Added own HTTP interacter, and made it super-sexy

--- lib/unfuddler.rb
+++ lib/unfuddler.rb
@@ -1,44 +1,65 @@
-require 'httparty'
 require 'hashie'
+require 'net/http'
+require 'crack/xml'
+require 'xmlsimple'
 
 module Unfuddler
-  include HTTParty
-  format :json
+  class << self
+    attr_accessor :username, :password, :subdomain, :http
 
-  def self.authenticate(info)
-    self.base_uri "#{info[:subdomain]}.unfuddle.com/api/v1"
-    self.basic_auth info[:username], info[:password]
+    def authenticate(info)
+      @username, @password, @subdomain = info[:username], info[:password], info[:subdomain]
+      @http = Net::HTTP.new("#{info[:subdomain]}.unfuddle.com")
+    end
+
+    def request(type, url, data = nil)
+      # Use Module#const_get (error before thus not using it now)
+      request = eval("Net::HTTP::#{type.capitalize}").new("/api/v1/#{url}.xml", {'Content-type:' => 'application/xml'})
+      request.basic_auth @username, @password
+
+      if [:put, :post].include?(type) and data
+        request.body = data
+      end
+
+      response = @http.request(request)
+      Crack::XML.parse(response.body)
+    end
+
+    def get(url)
+      request(:get, url)
+    end
+
+    def put(url, data)
+      request(:put, url, data)
+    end
   end
 
   class Project < Hashie::Mash
-    attr_accessor :attributes 
-
     def self.find
       projects = []
-      Unfuddler.get("/projects.json").each do |project|
+      Unfuddler.get("projects")["projects"].each do |project|
         projects << Project.new(project)
       end
       projects
     end
-    
+
     def tickets
-      Ticket.find(:all, {:project_id => id})
+      Ticket.find(self.id)
     end
   end
 
   class Ticket < Hashie::Mash
-    def self.find(which, options = {})
+    def self.find(project_id)
       tickets = []
-      Unfuddler.get("/projects/#{options[:project_id]}/tickets.json").each do |ticket|
-        tickets << Ticket.new(ticket)
+      Unfuddler.get("projects/#{project_id}/tickets")["tickets"].each do |project|
+        tickets << Ticket.new(project)
       end
       tickets
     end
 
-    def create
-      query = {:body => {:description => "Hello World"}}
-      Unfuddler.post("/projects/#{project_id}/tickets.json", query)
+    def save
+      ticket = XmlSimple.new("rootname" => "ticket")
+      Unfuddler.put("projects/#{project_id}/tickets/#{self.id}", ticket.xml_out(self.to_hash))
     end
   end
 end
-
